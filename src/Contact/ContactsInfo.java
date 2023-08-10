392048c74eec3213f70f93564a7ae78e12c173d2
Ava Smith <ava013005@gmail.com>
2023-08-10 15:35:28 -0500

    finished add function

--- src/Contact/ContactsInfo.java
+++ src/Contact/ContactsInfo.java
@@ -1,5 +1,7 @@
 package Contact;
 
+import util.Input;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,21 +12,63 @@ import java.util.*;
 
 public class ContactsInfo {
 
+    private static ArrayList<Contacts> contactList = new ArrayList<>();
+    private static Path p = Paths.get("src/Contact/contacts.txt");
+
     public static void main(String[] args) {
 
-        Path p = Paths.get("src/Contact/contacts.txt");
+
 
         Contacts bill = new Contacts("Billy Bob", "2103456784");
         Contacts sandy = new Contacts("Sandy Cheeks", "2103456784");
         Contacts tom = new Contacts("Tommy Tom", "2103456784");
+        Contacts jill = new Contacts("Jill Joe", "2106783456");
 
 
 
-        ArrayList<Contacts> contactList = new ArrayList<>();
-        contactList.add(bill);
-        contactList.add(sandy);
-        contactList.add(tom);
+//        ArrayList<Contacts> contactList = new ArrayList<>();
+//        contactList.add(bill);
+//        contactList.add(sandy);
+//        contactList.add(tom);
+//        contactList.add(jill);
 
+
+
+
+        while (true) {
+            Input input = new Input();
+            System.out.println("1. View contacts.\n2. Add a new contact. \n3. Search a contact by name. \n4. Delete an existing contact. \n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):");
+            int usersInput = input.getInt();
+            if (usersInput == 1) {
+                for (Contacts contact : contactList) {
+                    System.out.println(contact);
+                }
+            } else if (usersInput == 2) {
+                addNewContact();
+            } else if (usersInput == 3) {
+                searchContacts();
+            } else if (usersInput == 4) {
+                deleteContact();
+            } else {
+                break;
+            }
+        }
+    }
+
+//    public static void showAllContacts() {
+//        System.out.println(Contacts);
+//    }
+
+    //methods
+
+    public static void addNewContact() {
+        Input input = new Input();
+        System.out.println("Enter a name");
+        String usersName = input.getString();
+        System.out.println("Enter a phone number");
+        String usersPhoneNumber = input.getString();
+        Contacts newContact = new Contacts(usersName, usersPhoneNumber);
+        contactList.add(newContact);
         try{
             Set<String> existingNames = new HashSet<>(Files.readAllLines(p));
             for (Contacts contacts : contactList) {
@@ -36,37 +80,6 @@ public class ContactsInfo {
         } catch (IOException e){
             e.printStackTrace();
         }
-
-
-
-//        while (true) {
-//            Scanner sc = new Scanner(System.in);
-//            System.out.println("1. View contacts.\n2. Add a new contact. \n3. Search a contact by name. \n4. Delete an existing contact. \n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):");
-//            Integer usersInput = Integer.valueOf(sc.next());
-//            if (usersInput == 1) {
-//                for (Contacts contact : contactList) {
-//                    System.out.println(contact);
-//                }
-//            } else if (usersInput == 2) {
-//                addNewContact();
-//            } else if (usersInput == 3) {
-//                searchContacts();
-//            } else if (usersInput == 4) {
-//                deleteContact();
-//            } else {
-//                break;
-//            }
-//        }
-    }
-
-//    public static void showAllContacts() {
-//        System.out.println(Contacts);
-//    }
-
-    //methods
-
-    public static void addNewContact() {
-
     }
 
     public static void searchContacts(){
