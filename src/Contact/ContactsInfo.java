8497ca0809d217b2fdaf38717b607bedcaab50de
Ava Smith <ava013005@gmail.com>
2023-08-10 15:47:43 -0500

    finished functions

--- src/Contact/ContactsInfo.java
+++ src/Contact/ContactsInfo.java
@@ -17,32 +17,23 @@ public class ContactsInfo {
 
     public static void main(String[] args) {
 
-
-
         Contacts bill = new Contacts("Billy Bob", "2103456784");
         Contacts sandy = new Contacts("Sandy Cheeks", "2103456784");
         Contacts tom = new Contacts("Tommy Tom", "2103456784");
         Contacts jill = new Contacts("Jill Joe", "2106783456");
 
+        execute();
+    }
 
+    //methods
 
-//        ArrayList<Contacts> contactList = new ArrayList<>();
-//        contactList.add(bill);
-//        contactList.add(sandy);
-//        contactList.add(tom);
-//        contactList.add(jill);
-
-
-
-
+    public static void execute() {
         while (true) {
             Input input = new Input();
             System.out.println("1. View contacts.\n2. Add a new contact. \n3. Search a contact by name. \n4. Delete an existing contact. \n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):");
             int usersInput = input.getInt();
             if (usersInput == 1) {
-                for (Contacts contact : contactList) {
-                    System.out.println(contact);
-                }
+                showAllContacts();
             } else if (usersInput == 2) {
                 addNewContact();
             } else if (usersInput == 3) {
@@ -55,11 +46,19 @@ public class ContactsInfo {
         }
     }
 
-//    public static void showAllContacts() {
-//        System.out.println(Contacts);
-//    }
+    public static void showAllContacts() {
+        try {
+            List<String> lineOfTexts = Files.readAllLines(
+                    Paths.get("src/Contact/contacts.txt")
+            );
 
-    //methods
+            for(String text : lineOfTexts){
+                System.out.println(text);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     public static void addNewContact() {
         Input input = new Input();
@@ -75,7 +74,7 @@ public class ContactsInfo {
                 if (!existingNames.contains(contacts.getName())) {
                     Files.write(p, Collections.singletonList(contacts.getName()), StandardOpenOption.APPEND);
                     existingNames.add(contacts.getName());
-                    existingNames.add(contacts.getPhoneNumber());
+//                    existingNames.add(contacts.getPhoneNumber());
                 }
             }
         } catch (IOException e){
