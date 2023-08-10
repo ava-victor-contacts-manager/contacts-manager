c5b9154776a8ff633b1ec5bf33abdbc13e92f752
Ava Smith <ava013005@gmail.com>
2023-08-10 11:59:39 -0500

    finished stuff

--- src/Contact/Contacts.java
+++ src/Contact/Contacts.java
@@ -3,26 +3,32 @@ package Contact;
 import java.util.ArrayList;
 
 public class Contacts {
-
     // instance variables
     private String name;
-
-
-    private ArrayList<Integer> phoneNumber;
+    private long phoneNumber;
 
     //getters
     public String getName() {
         return name;
     }
-
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
 
     //setters
     public void setName(String name) {
         this.name = name;
     }
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // constructor
+    public Contacts(String name, long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     // methods
-
-    //constructor
 
 }
--- src/Contact/ContactsInfo.java
+++ src/Contact/ContactsInfo.java
@@ -1,10 +1,13 @@
 package Contact;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ContactsInfo {
     public static void main(String[] args) {
 
-                    System.out.println("1. View contacts.\n2. Add a new contact. \n3. Search a contact by name. \n4. Delete an existing contact. \n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):");
+//                    System.out.println("1. View contacts.\n2. Add a new contact. \n3. Search a contact by name. \n4. Delete an existing contact. \n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):");
 
 //        while (true) {
 //            Scanner sc = new Scanner(System.in);
@@ -13,6 +16,20 @@ public class ContactsInfo {
 //                System.out.print();
 //            }
 //        }
-    }
+//    }
 
+
+        Contacts bill = new Contacts("Billy Bob", 2103456784);
+        Contacts sandy = new Contacts("Sandy Cheeks", 2103456784);
+        Contacts tom = new Contacts("Tommy Tom", 2103456784);
+
+        ArrayList<Contacts> contactList = new ArrayList<>();
+        contactList.add(bill);
+        contactList.add(sandy);
+        contactList.add(tom);
+
+//        System.out.println(contactList);
+        System.out.println(bill.getName());
+        System.out.println(bill.getPhoneNumber());
+    }
 }
